d063b5c39b16f4227b29fedba9ff995688ca8307
Vivekanand P V <vivekanandpv@outlook.com>
2020-07-08 21:29:01 +0530

    initial commit

---
+++ src/test/java/com/example/junitlearning/JunitLearningApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.junitlearning;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JunitLearningApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
