aac8d019d2e7b4b6a2c62909563e62f247b12e9a
Vivekanand P V <vivekanandpv@outlook.com>
2020-07-08 22:07:29 +0530

    done

--- src/test/java/com/example/junitlearning/repositories/InvoiceRepositoryTest.java
+++ src/test/java/com/example/junitlearning/repositories/InvoiceRepositoryTest.java
@@ -9,40 +9,27 @@ import static org.junit.jupiter.api.Assertions.*;
 
 class InvoiceRepositoryTest {
 
-    @BeforeAll
-    static void beforeAll() {
-        System.out.println("Start Test Class");
-    }
-
-    @AfterAll
-    static void afterAll() {
-        System.out.println("End of Test Class");
-    }
-
     @BeforeEach
     void setUp() {
-        System.out.println("Initializing test case");
     }
 
     @AfterEach
     void tearDown() {
-        System.out.println("Finished test case");
     }
 
     @Test
     void getList() {
-        //  1. Arrange
         InvoiceRepository repository = new InvoiceRepository();
-
-        //  2. Act
         List<Invoice> list = repository.getList();
-
-        //  3. Assert
         assertNotNull(list);
     }
 
     @Test
     void addInvoice() {
+        InvoiceRepository repository = new InvoiceRepository();
+        assertEquals(0, repository.getList().size());
+        repository.addInvoice(new Invoice());
+        assertEquals(1, repository.getList().size());
     }
 
     @Test
