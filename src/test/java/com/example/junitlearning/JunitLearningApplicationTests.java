daa70af8ed24933660da467941248ba18f0c26f6
Vivekanand P V <vivekanandpv@outlook.com>
2020-07-09 15:23:28 +0530

    tags

--- src/test/java/com/example/junitlearning/JunitLearningApplicationTests.java
+++ src/test/java/com/example/junitlearning/JunitLearningApplicationTests.java
@@ -1,9 +1,11 @@
 package com.example.junitlearning;
 
+import org.junit.jupiter.api.Tag;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
+@Tag("ABC")
 class JunitLearningApplicationTests {
 
     @Test
--- src/test/java/com/example/junitlearning/repositories/InvoiceRepositoryTest.java
+++ src/test/java/com/example/junitlearning/repositories/InvoiceRepositoryTest.java
@@ -9,6 +9,7 @@ import java.util.List;
 import static org.junit.jupiter.api.Assertions.*;
 
 @DisplayName("InvoiceRepository should")
+@Tag("WIP")
 class InvoiceRepositoryTest {
 
     @BeforeEach
